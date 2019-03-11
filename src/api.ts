96b1481339e729361e31adc41e647f4b99643e6e
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-03-11 20:23:12 +0100

    Removed express-graphql, added apollo-server and apollo-server-express.

--- src/api.ts
+++ src/api.ts
@@ -1,77 +1,74 @@
+import { gql } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 import express from 'express'
-import expressGraphql = require('express-graphql')
-import { buildSchema } from 'graphql'
 import { cacheTimestamp, data } from '.'
 
-const schema = buildSchema(`
-"""
-A data value for a given type of pollen, for a given city
-"""
-type DataValue {
+const typeDefs = gql`
   """
-  The type of pollen
+  A data value for a given type of pollen, for a given city
   """
-  type: String!
-  """
-  The amount of that pollen per square meter
-  """
-  value: Int
-}
+  type DataValue {
+    """
+    The type of pollen
+    """
+    type: String!
+    """
+    The amount of that pollen per square meter
+    """
+    value: Int
+  }
 
-"""
-Data for a given city, whose name is found in the city field
-"""
-type DataElem {
   """
-  The city, that the measurement is from
+  Data for a given city, whose name is found in the city field
   """
-  city: String!
-  """
-  The text forecast provided for the city
-  """
-  forecast: String
-  """
-  The separate pollen values for each type of pollen
-  """
-  values: [DataValue!]!
-}
+  type DataElem {
+    """
+    The city, that the measurement is from
+    """
+    city: String!
+    """
+    The text forecast provided for the city
+    """
+    forecast: String
+    """
+    The separate pollen values for each type of pollen
+    """
+    values: [DataValue!]!
+  }
 
-type Query {
-  """
-  When the data is from, as a ISO8601 string
-  """
-  cacheTimestamp: String
-  """
-  The currently available data, for the various cities
-  """
-  data: [DataElem!]
-}
-`)
+  type Query {
+    """
+    When the data is from, as a ISO8601 string
+    """
+    cacheTimestamp: String
+    """
+    The currently available data, for the various cities
+    """
+    data: [DataElem!]
+  }
+`
 
-const resolver = {
-  data: () => {
-    if (data == null) {
-      return data
-    }
-    return data.map(city => ({
-      ...city,
-      values: Object.entries(city.values).map(([key, value]) => ({
-        type: key,
-        value: value < 0 ? null : value
+const resolvers = {
+  Query: {
+    data: () => {
+      if (data == null) {
+        return data
+      }
+      return data.map(city => ({
+        ...city,
+        values: Object.entries(city.values).map(([key, value]) => ({
+          type: key,
+          value: value < 0 ? null : value
+        }))
       }))
-    }))
-  },
-  cacheTimestamp: () =>
-    cacheTimestamp == null ? cacheTimestamp : cacheTimestamp.toISOString()
+    },
+    cacheTimestamp: () =>
+      cacheTimestamp == null ? cacheTimestamp : cacheTimestamp.toISOString()
+  }
 }
+
+const server = new ApolloServer({ typeDefs, resolvers })
 
 export default (app: express.Express) => {
-  app.use(
-    '/graphql',
-    expressGraphql({
-      schema,
-      rootValue: resolver,
-      graphiql: true
-    })
-  )
+  server.applyMiddleware({ app })
 }
