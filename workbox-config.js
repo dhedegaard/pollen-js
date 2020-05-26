f6449bc20099e7bcbb380b7fb873f732c6ab8ab3
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-05-26 19:51:25 +0200

    Added runtime caching of the fonts and root html.

--- workbox-config.js
+++ workbox-config.js
@@ -1,9 +1,18 @@
 module.exports = {
-  "globDirectory": "static/",
-  "globPatterns": [
-    "**/*.{png,ico,json,js,woff2}"
+  globDirectory: 'static/',
+  globPatterns: ['**/*.{png,ico,json,js,woff2}'],
+  swDest: 'static/sw.js',
+  clientsClaim: true,
+  skipWaiting: true,
+  mode: 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /\/$/,
+      handler: 'NetworkFirst',
+    },
+    {
+      urlPattern: /.woff2$/,
+      handler: 'CacheFirst',
+    },
   ],
-  "swDest": "static/sw.js",
-  "clientsClaim": true,
-  "skipWaiting": true
-};
+}
