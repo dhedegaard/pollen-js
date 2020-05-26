5dea847de4de14fbcbbdb836fd87e2cd03a12144
Dennis Hedegaard <dennis@dhedegaard.dk>
2020-05-26 18:31:53 +0200

    Initial build for workbox service worker.

---
+++ workbox-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "globDirectory": "static/",
+  "globPatterns": [
+    "**/*.{png,ico,json,js,woff2}"
+  ],
+  "swDest": "static/sw.js",
+  "clientsClaim": true,
+  "skipWaiting": true
+};
