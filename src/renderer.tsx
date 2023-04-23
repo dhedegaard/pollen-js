54b8a08d7f1cde23f570666313d84f239c443ff7
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-04-23 20:16:42 +0200

    render to static HTML in SSR, to avoid rendering the hydration attributes.

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,12 +1,12 @@
-import { ParsedXMLStructure } from './parser'
-import { renderToString } from 'react-dom/server'
+import { renderToStaticMarkup } from 'react-dom/server'
 import Index from './pages'
+import { ParsedXMLStructure } from './parser'
 
 export const renderDataToHTML = (
   data: ParsedXMLStructure[] | undefined,
   cacheTimestamp: Date | undefined
 ): string => {
-  const html = renderToString(
+  const html = renderToStaticMarkup(
     <Index data={data} cacheTimestamp={cacheTimestamp} />
   )
   return `<!doctype html>${html}`
