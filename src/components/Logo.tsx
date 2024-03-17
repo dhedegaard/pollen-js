71f595ed67d9b48151f73947242a0d2c56c2a3d5
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-03-17 11:21:07 +0100

    simplify syntax

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,12 @@
-import { FC, useMemo } from 'react'
+import { FC } from 'react'
 import logoPng from '../../public/logo.png'
 import logo2xPng from '../../public/logo@2x.png'
 
 export const Logo: FC = () => {
-  const srcSet = useMemo(() => `${logoPng.src} 1x, ${logo2xPng.src} 2x`, [])
-
   return (
     <img
       src={logoPng.src}
-      srcSet={srcSet}
+      srcSet={`${logoPng.src} 1x, ${logo2xPng.src} 2x`}
       width="30"
       height="30"
       alt="Pollen icon"
