23cf7139387b6b7de8a876003e8774bd8250d346
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-03-19 18:33:44 +0100

    old unused script

--- public/main.js
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-// Reload the page whenever it becomes visible, fixes reload issues in iOS when
-// running as a webapp.
-window.document.addEventListener('visibilitychange', (event) => {
-  if (window.document.visibilityState === 'visible') {
-    window.location.reload()
-  }
-})
