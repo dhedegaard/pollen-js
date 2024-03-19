0151d0ff7067bbb21c21c7026d50ae7ad0b5a570
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-03-19 19:10:44 +0100

    better timestamp + skeleton

--- src/app/CacheTimestamp.tsx
+++ src/app/CacheTimestamp.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import clsx from 'clsx'
 import { memo, useMemo } from 'react'
 import { DataActionResult } from '../actions/data-action'
 import { useData } from '../actions/data-fetcher'
@@ -11,14 +12,25 @@ interface Props {
 export const CacheTimestamp = memo(function CacheTimestamp({
   initialData,
 }: Props) {
-  const { data } = useData()
+  const { data, isLoading } = useData()
   const timestamp = useMemo(
-    () => new Date(data?.timestamp ?? initialData.timestamp).toUTCString(),
+    () =>
+      new Date(data?.timestamp ?? initialData.timestamp).toLocaleString(
+        'en-GB'
+      ),
     [data?.timestamp, initialData.timestamp]
   )
   return (
     <div>
-      Cache timestamp: <span className="font-medium">{timestamp}</span>
+      Cache timestamp:{' '}
+      <span
+        className={clsx(
+          'font-medium',
+          isLoading && 'skeleton rounded bg-slate-100 text-transparent ml-auto'
+        )}
+      >
+        {timestamp}
+      </span>
     </div>
   )
 })
