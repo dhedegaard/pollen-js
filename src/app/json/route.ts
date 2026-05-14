03b7f40d27b0a462dd3174110deebcf29e66de35
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-14 15:34:49 +0200

    no need to catch and throw, as the result is the same

--- src/app/json/route.ts
+++ src/app/json/route.ts
@@ -2,11 +2,6 @@ import { NextResponse } from 'next/server'
 import { getData } from '../../actions/data-action'
 
 export const GET = async () => {
-  try {
-    const data = await getData()
-    return NextResponse.json(data)
-  } catch (error: unknown) {
-    console.error(error)
-    return new Response('Internal Server Error', { status: 500 })
-  }
+  const data = await getData()
+  return NextResponse.json(data)
 }
